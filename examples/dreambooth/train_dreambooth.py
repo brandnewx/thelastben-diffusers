ed579632aaae17363e3a8b8277e033761998b5f5
Sopheap Ly <brandnewx@gmail.com>
2022-12-01 15:10:36 +0000

    ddim

--- examples/dreambooth/train_dreambooth.py
+++ examples/dreambooth/train_dreambooth.py
@@ -776,7 +776,7 @@ def main():
                         if args.scheduler_name == "DDIM":
                             scheduler=DDIMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", clip_sample=False, set_alpha_to_one=False)
                         else:
-                            raise ValueError("Invalid scheduler: " + args.scheduler_name)
+                            raise ValueError("Invalid --scheduler_name=" + args.scheduler_name)
                             
                         pipeline = StableDiffusionPipeline.from_pretrained(
                             args.pretrained_model_name_or_path,
@@ -843,7 +843,7 @@ def main():
             if args.scheduler_name == "DDIM":
                 scheduler=DDIMScheduler(beta_start=0.00085, beta_end=0.012, beta_schedule="scaled_linear", clip_sample=False, set_alpha_to_one=False)
             else:
-                raise ValueError("Invalid scheduler: " + args.scheduler_name)
+                raise ValueError("Invalid --scheduler_name=" + args.scheduler_name)
 
             pipeline = StableDiffusionPipeline.from_pretrained(
                 args.pretrained_model_name_or_path,
